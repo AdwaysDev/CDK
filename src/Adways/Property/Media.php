c866fe5e97d17d7ef7d21839be9c47ec34188d95
David THIBAULT <dahooo2@gmail.com>
2017-06-20 10:46:57 +0200

    maj media

--- src/Adways/Property/Media.php
+++ src/Adways/Property/Media.php
@@ -24,88 +24,62 @@ class Media extends Property implements MediaInterface {
     public function __construct($key, $label = '', $tooltip = '', $representation = null, $defaultValue = '', $reloadPageOnChange = true, $reloadPropertiesOnChange = false, $options = null) {
         parent::__construct($key, $label, $tooltip, $representation, $defaultValue, $reloadPageOnChange, $reloadPropertiesOnChange, $options);
     }
+
     public function setValue($value) {
-        $this->value = json_decode((string) $value);
-        $this->mime = $this->value->mime;
-        $this->xkey = $this->value->xkey;
-        $this->path = $this->value->path;
-        $this->name = $this->value->name;
-        $this->id = $this->value->id;
-        
-        $mediaElementsCount = 0;
-        foreach ($this->value->assets as $asset) {
-            $mediaElement = new MediaElement($this->id+'_mediaElement_'+$mediaElementsCount, '', '', Representations::_DEFAULT, $asset);
-            $this->assets[] = $mediaElement;
-            $mediaElementsCount++;
+        if ($value !== '') {
+            $this->value = json_decode((string) $value);
+            $this->mime = $this->value->mime;
+            $this->xkey = $this->value->xkey;
+            $this->path = $this->value->path;
+            $this->name = $this->value->name;
+            $this->id = $this->value->id;
+
+            $mediaElementsCount = 0;
+            foreach ($this->value->assets as $asset) {
+                $mediaElement = new MediaElement($this->id + '_mediaElement_' + $mediaElementsCount, '', '', Representations::_DEFAULT, $asset);
+                $this->assets[] = $mediaElement;
+                $mediaElementsCount++;
+            }
+
+            $thumbnailCount = 0;
+            foreach ($this->value->thumbnails as $thumbnail) {
+                $mediaElement = new MediaElement($this->id + '_thumbnail_' + $thumbnailCount, '', '', Representations::_DEFAULT, $thumbnail);
+                $this->thumbnails[] = $mediaElement;
+                $thumbnailCount++;
+            }
         }
-        
-        $thumbnailCount = 0;	
-        foreach ($this->value->thumbnails as $thumbnail) {
-            $mediaElement = new MediaElement($this->id+'_thumbnail_'+$thumbnailCount, '', '', Representations::_DEFAULT, $thumbnail);
-            $this->thumbnails[] = $mediaElement;
-            $thumbnailCount++;
-        }	
     }
+
     public function setDefaultValue($newValue) {
         $this->defaultValue = (string) $newValue;
     }
+
     public function getMime() {
         return $this->mime;
     }
+
     public function getXKey() {
         return $this->xkey;
     }
+
     public function getPath() {
         return $this->path;
     }
+
     public function getThumbnails() {
         return $this->thumbnails;
     }
+
     public function getAssets() {
         return $this->assets;
     }
+
     public function getName() {
         return $this->name;
     }
+
     public function getId() {
         return $this->id;
     }
+
 }
-/*
- "{"mime":"video/x-adways","xkey":null,"path":"Haribo TAGADA - Pub 2016 - Kids Voices.mp4","thumbnails":["607","608"],"assets":[{"state":1,"location":"https://d2zmjmh9pxcrfq.cloudfront.net/64/assets/bcpfeaz-output-720-auto.mp4","mime":"video/mp4","width":1280,"height":720,"ratio":1.778,"size":6133876,"duration":30,"framerate":"29.97","bitrate":0,"id":605,"created":"2017-06-19 14:33:47","author":1,"updated":"2017-06-19 14:33:47","updator":1,"level":15,"min_bitrate":200,"max_bitrate":1300,"buffer_size":13000,"_links":{"self":{"href":"http://192.168.1.110/adways/services/public/media-element/605"}}},{"state":1,"location":"https://d2zmjmh9pxcrfq.cloudfront.net/64/assets/bcpfeaz-output-360-auto.mp4","mime":"video/mp4","width":640,"height":360,"ratio":1.778,"size":3952227,"duration":30,"framerate":"29.97","bitrate":0,"id":606,"created":"2017-06-19 14:33:48","author":1,"updated":"2017-06-19 14:33:48","updator":1,"level":15,"min_bitrate":200,"max_bitrate":1300,"buffer_size":13000,"_links":{"self":{"href":"http://192.168.1.110/adways/services/public/media-element/606"}}}],"name":"Haribo TAGADA - Pub 2016 - Kids Voices.mp4","state":1,"id":64,"created":"2017-06-19 14:33:22","author":1,"updated":"2017-06-19 14:33:53","updator":1,"level":15,"_links":{"self":{"href":"http://192.168.1.110/adways/services/public/media/64"}}}" 
- */
-/*
-{
-    "mime":"video/x-adways",
-     "xkey":null,
-     "path":"Haribo TAGADA - Pub 2016 - Kids Voices.mp4",
-     "thumbnails":["607", "608"],
-     "assets":[
-        {
-            "location":"https://d2zmjmh9pxcrfq.cloudfront.net/64/assets/bcpfeaz-output-720-auto.mp4",
-             "mime":"video/mp4",
-             "width":1280,
-             "height":720,
-             "ratio":1.778,
-             "size":6133876,
-             "duration":30,
-             "framerate":"29.97",
-             "bitrate":0,
-             "id":605 
-        },
-         {
-            "location":"https://d2zmjmh9pxcrfq.cloudfront.net/64/assets/bcpfeaz-output-360-auto.mp4",
-             "mime":"video/mp4",
-             "width":640,
-             "height":360,
-             "ratio":1.778,
-             "size":3952227,
-             "duration":30,
-             "framerate":"29.97",
-             "bitrate":0,
-             "id":606
-         }
-    ],
-     "name":"Haribo TAGADA - Pub 2016 - Kids Voices.mp4",
-     "id":64
-}*/
--- src/Adways/Property/MediaElement.php
+++ src/Adways/Property/MediaElement.php
@@ -26,64 +26,65 @@ class MediaElement extends Property implements MediaElementInterface {
     public function __construct($key, $label = '', $tooltip = '', $representation = null, $defaultValue = '', $reloadPageOnChange = true, $reloadPropertiesOnChange = false, $options = null) {
         parent::__construct($key, $label, $tooltip, $representation, $defaultValue, $reloadPageOnChange, $reloadPropertiesOnChange, $options);
     }
+
     public function setValue($value) {
-        $this->value = $value;
-        $this->location = $this->value->location;
-        $this->mime = $this->value->mime;
-        $this->width = $this->value->width;
-        $this->height = $this->value->height;
-        $this->ratio = $this->value->ratio;
-        $this->size = $this->value->size;
-        $this->duration = $this->value->duration;
-        $this->framerate = $this->value->framerate;
-        $this->bitrate = $this->value->bitrate;
-        $this->id = $this->value->id;	
-    }    
+        if ($value !== '') {
+            $this->value = $value;
+            $this->location = $this->value->location;
+            $this->mime = $this->value->mime;
+            $this->width = $this->value->width;
+            $this->height = $this->value->height;
+            $this->ratio = $this->value->ratio;
+            $this->size = $this->value->size;
+            $this->duration = $this->value->duration;
+            $this->framerate = $this->value->framerate;
+            $this->bitrate = $this->value->bitrate;
+            $this->id = $this->value->id;
+        }
+    }
+
     public function setDefaultValue($newValue) {
         $this->defaultValue = $newValue;
     }
+
     public function getLocation() {
         return $this->location;
     }
+
     public function getMime() {
         return $this->mime;
     }
+
     public function getWidth() {
         return $this->width;
     }
+
     public function getHeight() {
         return $this->height;
     }
+
     public function getRatio() {
         return $this->ratio;
     }
+
     public function getSize() {
         return $this->size;
     }
+
     public function getDuration() {
         return $this->duration;
     }
+
     public function getFramerate() {
         return $this->framerate;
     }
+
     public function getBitrate() {
         return $this->bitrate;
     }
+
     public function getId() {
         return $this->id;
     }
+
 }
-/*
-{
-   "location":"https://d2zmjmh9pxcrfq.cloudfront.net/64/assets/bcpfeaz-output-360-auto.mp4",
-    "mime":"video/mp4",
-    "width":640,
-    "height":360,
-    "ratio":1.778,
-    "size":3952227,
-    "duration":30,
-    "framerate":"29.97",
-    "bitrate":0,
-    "id":606
-}
-}*/
