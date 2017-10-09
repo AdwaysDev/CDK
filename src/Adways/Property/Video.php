098becad61ad53fa3a5e7e24b499f9f259424642
David THIBAULT <dahooo2@gmail.com>
2017-10-09 17:27:33 +0200

    maj prop video

--- src/Adways/Property/Video.php
+++ src/Adways/Property/Video.php
@@ -18,8 +18,10 @@ class Video extends NodeSet implements MediaElementInterface {
     protected $selectionKind = null;
     protected $selectionProperty = null;
     protected $mp4URLProperty = null;
+    protected $assetsSelectionProperty = null;
+    protected $useAssets = false;
 
-    public function __construct($key, $label = '', $tooltip = '', $defaultValue = '//d3iuja8fgpny0q.cloudfront.net/studio/Video_base_V13.mp4') {
+    public function __construct($key, $label = '', $tooltip = '', $defaultValue = '//d3iuja8fgpny0q.cloudfront.net/studio/Video_base_V13.mp4', $useAssets = false) {
         parent::__construct($key, $label, $tooltip, Representations::_DEFAULT, $defaultValue, true, false, false);
 
         $this->selectionKind = Array(
@@ -29,100 +31,178 @@ class Video extends NodeSet implements MediaElementInterface {
         $this->selectionProperty = new SimpleSelection('selectionKind_' . $key, 'Video type', '', Representations::_DEFAULT, $this->selectionKind, $this->selectionKind[0], true, true);
 //
         $this->addProperty($this->selectionProperty);
+        $this->useAssets = $useAssets;
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             $this->mp4URLProperty = new Characters('mp4_' . $key, 'URL', '', Representations::_DEFAULT, $defaultValue);
             $this->addProperty($this->mp4URLProperty);
         } else {
             $this->mediaProperty = new Media('media_' . $key, 'Upload Media', '', Representations::_DEFAULT, '');
             $this->addProperty($this->mediaProperty);
+            
+            if($this->useAssets && count($this->mediaProperty->getAssets()) > 0) {
+                $assets = Array();
+                $mediaElementsCount = 0;
+                foreach ($this->mediaProperty->getAssets() as $value){
+                  $assets[] = array('key' => $mediaElementsCount, 'value' => $value->getMime().' '.$value->getWidth().' x '.$value->getHeight());
+                    $mediaElementsCount++;
+                }   
+                $this->assetsSelectionProperty = new SimpleSelection('assetsSelection_' . $value->getId(), 'Assets', '', Representations::_DEFAULT, $assets, $assets[0], true, true);
+                $this->addProperty($this->assetsSelectionProperty);
+            }
         }
     }
 
     public function getLocation() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return $this->mp4URLProperty->getValue();
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getLocation();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getLocation() : '');
-        }
+            return '';
+        } 
     }
 
     public function getThumbnail() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return '';
-        } else {
-            return (count($this->mediaProperty->getThumbnails()) > 0 ? $this->mediaProperty->getThumbnails()[0]->getLocation() : '');
+        } else if (count($this->mediaProperty->getThumbnails()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getThumbnails()[$assetId]->getLocation();
+        }  else {
+            return '';
         }
     }
 
     public function getMime() {        
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 'video/mp4';
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getMime();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getMime() : '');
-        }
+            return '';
+        } 
     }
 
     public function getWidth() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getWidth();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getWidth() : 0);
+            return 0;
         }
     }
 
     public function getHeight() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getHeight();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getHeight() : 0);
+            return 0;
         }
     }
 
     public function getRatio() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 1;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getRatio();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getRatio() : 1);
+            return 1;
         }
     }
 
     public function getSize() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getSize();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getSize() : 0);
+            return 0;
         }
     }
 
     public function getDuration() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getDuration();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getDuration() : 0);
+            return 0;
         }
     }
 
     public function getFramerate() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getFramerate();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getFramerate() : 0);
+            return 0;
         }
     }
 
     public function getBitrate() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return 0;
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getBitrate();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getFramerate() : 0);
+            return 0;
         }
     }
 
     public function getId() {
         if ($this->selectionProperty->getValue()['key'] == 'url') {
             return $this->mp4URLProperty->getValue();
+        } else if (count($this->mediaProperty->getAssets()) > 0) {
+            $assetId = 0;
+            if($this->useAssets) {
+                $assetId = $this->assetsSelectionProperty->getValue()['key'];
+            } 
+            return $this->mediaProperty->getAssets()[$assetId]->getId();
         } else {
-            return (count($this->mediaProperty->getAssets()) > 0 ? $this->mediaProperty->getAssets()[0]->getId() : 0);
+            return 0;
         }
     }
 }
