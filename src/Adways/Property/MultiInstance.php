15b3f2fa94f34a619899179a42ead367f025af47
Briiiiiiiiiiian Rivière <brian.riviere@adways.com>
2017-11-20 11:12:54 +0100

    modif prop multi instance v3

--- src/Adways/Property/MultiInstance.php
+++ src/Adways/Property/MultiInstance.php
@@ -10,28 +10,28 @@ use Adways\Property\Boolean;
  */
 
 class MultiInstance extends NodeSet implements MultiInstanceInterface
-{		
-	protected $kind = '';
-	protected $numberOfEntry = 0;
-	 
-	public function __construct($key, $label = '', $tooltip = '', $kind = '', $defaultValues = array()) {
-        parent::__construct($key, $label, $tooltip, Representations::_DEFAULT, '', true, false, false);	
+{       
+    protected $kind = '';
+    protected $numberOfEntry = 0;
+     
+    public function __construct($key, $label = '', $tooltip = '', $kind = '', $defaultValues = array()) {
+        parent::__construct($key, $label, $tooltip, Representations::_DEFAULT, '', true, false, false); 
         $this->type = ContentTemplateRPC::PROPERTY_TYPE_MULTI_INSTANCE;
-		$this->kind = $kind;        
+        $this->kind = $kind;        
         $this->numberOfEntry = new Number('number_of_value_' . $key, 'NumberOfEntry', '', Representations::_DEFAULT, count($defaultValues));
         $this->addProperty($this->numberOfEntry);
         for ($i = 0; $i < $this->numberOfEntry->getValue(); $i++) {
             switch ($this->kind) {
                 case ContentTemplateRPC::PROPERTY_TYPE_STRING:
-                    $entry = new Characters('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT);
+                    $entry = new Characters('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT, $defaultValues[$i]);
                     break;
                 case ContentTemplateRPC::PROPERTY_TYPE_NUMBER:
-                    $entry = new Number('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT);
+                    $entry = new Number('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT, $defaultValues[$i]);
                     break;
 //                case ContentTemplateRPC::PROPERTY_TYPE_MEDIA:
 //                    break;
                 case ContentTemplateRPC::PROPERTY_TYPE_BOOLEAN:
-                    $entry = new Boolean('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT);
+                    $entry = new Boolean('value_' . $key . '_' .$i, 'Entry ' . $i, '', Representations::_DEFAULT, $defaultValues[$i]);
                     break;
 //                case ContentTemplateRPC::PROPERTY_TYPE_TIME:
 //                    break;
@@ -44,17 +44,17 @@ class MultiInstance extends NodeSet implements MultiInstanceInterface
             }
             $this->addProperty($entry);
         }
-	}
+    }
 
     public function getValue() {
         return $this->propertiesToArray();
     }
-	
-	public function getInstanceKind() {	
-		return $this->kind; 
-	}   
-	
-	public function getNumberOfEntry() {	
-		return $this->numberOfEntry; 
-	}     
+    
+    public function getInstanceKind() { 
+        return $this->kind; 
+    }   
+    
+    public function getNumberOfEntry() {    
+        return $this->numberOfEntry; 
+    }     
 }  
