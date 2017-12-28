d61a9d01508c822a6290133005b9469c7af709f3
Briiiiiiiiiiian Rivière <brian.riviere@adways.com>
2017-12-28 11:46:15 +0100

    modification classe template pour tags

--- src/Adways/StudioTemplate/Template.php
+++ src/Adways/StudioTemplate/Template.php
@@ -14,29 +14,36 @@ class Template extends MasterTemplate implements TemplateInterface {
     
     private $permanentEntity = false;
     private $studioUIConfig = array();
+    private $tags = array();
     
-    public function __construct($config = array()) {		
+    public function __construct($config = array()) {        
         parent::__construct($config);
-	}
+    }
         
-	public function setPermanentEntity($permanentEntity){
+    public function setPermanentEntity($permanentEntity){
         $this->permanentEntity = (boolean) $permanentEntity;        
     }      
-	public function getPermanentEntity(){
+    public function getPermanentEntity(){
         return $this->permanentEntity;
     } 
-	public function getStudioUIConfig(){
+    public function getStudioUIConfig(){
         return $this->studioUIConfig;
     }    
     public function addStudioUIConfig($key, $value) {
         $this->studioUIConfig[$key] = $value;
+    }
+    public function addTag($tag) {
+        $this->tags[] = $tag;
+    }
+    public function removeTag($tag) {
+        array_splice($this->tags, array_search($tag, $this->tags), 1);
     }
     
     protected function getData() {        
         $data =  parent::getData();      
         $data[ContentTemplateRPC::CONTENT_PERMANENT_ENTITY] = $this->permanentEntity;   
         $data[ContentTemplateRPC::CONTENT_STUDIO_UI] = $this->studioUIConfig;
+        $data[ContentTemplateRPC::CONTENT_STUDIO_ENTITY_TAGS] = $this->tags;
         return $data;
     }
 }
-
