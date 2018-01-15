cbf458c6cf76d51f95810ed75de050073872c13d
Briiiiiiiiiiian Rivière <brian.riviere@adways.com>
2018-01-15 10:04:01 +0100

    ajout tags

--- src/Adways/Content/Tags.php
+++ src/Adways/Content/Tags.php
@@ -12,13 +12,19 @@ class Tags
 {   
     const DEVICE_DESKTOP = "101";
     const DEVICE_MOBILE = "102";
+    const DEVICE_ANDROID = "118";
+    const DEVICE_IOS = "119";
+    
     const TYPE_VAST = "103";
     const TYPE_VAST_VPAID = "104";
     const TYPE_VPAID = "105";
     const TYPE_MRAID = "106";
     const TYPE_ZIP = "107";
     const TYPE_HTML = "108";
-    const TYPE_SCRIPT = "109";
+    // const TYPE_SCRIPT = "109";
+    const TYPE_MRAID_READY = "116";
+    const TYPE_TAG_JS = "117";
+
     const KIND_LINEAR = "110";
     const KIND_NONLINEAR = "111";
     const KIND_COMPANION = "112";
